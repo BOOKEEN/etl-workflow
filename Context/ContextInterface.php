7b1eb476bf97528dd80beae550e2861903a2b982
melanie <melaniev@booken.com>
2015-08-07 11:53:34 +0200

    ajout attributs dans ContextInterface

--- Context/ContextInterface.php
+++ Context/ContextInterface.php
@@ -2,6 +2,24 @@
 
 namespace Bookeen\ETLWorkflow\Context;
 
+/**
+ * The context is created to be some information that will be passed to each function within workflow::process
+ * Interface ContextInterface
+ * @package Bookeen\ETLWorkflow\Context
+ */
 interface ContextInterface
 {
+    /**
+     * @param String $key
+     * @return mixed
+     */
+    public function getData($key);
+
+    /**
+     * you should verify the key here to be sur that you only have what you accept.
+     * @param string $key
+     * @param $val
+     * @return mixed
+     */
+    public function setData($key, $val);
 }
